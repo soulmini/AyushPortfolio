4c35831d31068bb7afc8cd503aff534d7bfc0335
ayush6999 <ayushjaiswal6999@gmail.com>
2024-03-20 16:33:57 +0530

    some changes in navbar

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,22 +1,37 @@
 import Link from 'next/link';
 
-
-function Navbar () {
-    return <>
-        <div className="flex items-center justify-between bg-opacity-50 backdrop-filter backdrop-blur-lg bg-black-200 py-4 px-6 fixed top-0 w-full z-50">
-        <div className="flex items-center justify-center">
-            <h1 className="text-200 font-bold text-gray-1000 hover:text-gray-400">Ayush Jaiswal</h1>
-        </div>
+function Navbar() {
+  return (
+    <div className="bg-black bg-opacity-90 py-4 px-6 fixed top-0 w-full z-50">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl md:text-3xl font-bold text-gray-100 hover:text-gray-400 transition duration-300">
+          Ayush Jaiswal
+        </h1>
         <div className="flex items-center space-x-6">
-            <a href="#" className="text-white-800 font-bold hover:text-gray-400 transition duration-300">Resume</a>
-            <a href="#" className="text-white-800 font-bold hover:text-gray-400 transition duration-300">About</a>
-            <a href="#" className="text-white-800 font-bold hover:text-gray-400 transition duration-300">Skills</a>
-            <a href="#" className="text-white-800 font-bold hover:text-gray-400 transition duration-300">Contact</a>
+          <Link href="/">
+            <span className="text-white font-semibold hover:text-gray-400 transition duration-300">
+              Resume
+            </span>
+          </Link>
+          <Link href="/about">
+            <span className="text-white font-semibold hover:text-gray-400 transition duration-300">
+              About
+            </span>
+          </Link>
+          <Link href="/skills">
+            <span className="text-white font-semibold hover:text-gray-400 transition duration-300">
+              Skills
+            </span>
+          </Link>
+          <Link href="/contact">
+            <span className="text-white font-semibold hover:text-gray-400 transition duration-300">
+              Contact
+            </span>
+          </Link>
         </div>
-</div>
-
-    </>
+      </div>
+    </div>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
